06ca73dfd72d38513af2ef598918e5629385ddc1
peakcm <coreypeak@gmail.com>
2016-11-14 07:30:51 -0500

    Generalize model a little more

--- src/Run_SIRV_model.R
+++ src/Run_SIRV_model.R
@@ -7,8 +7,8 @@ run_model <- function(inits, func, times, params){
              func = func,
              parms = params)
   run <- data.frame(run)
-  names(run) <- c("time", "S", paste0("V.", 1:params$n.comps.V), "E", "I", "R", "W", "CI")
-  run$V_total <- apply(run[,3:(2+params$n.comps.V)], 1, sum)
+  names(run) <- c("time", "S", paste0("V.", 1:params$n.comps.V), "E", "I", "R", "W", "CI", "Vax")
+  run$V_total <- apply(run[,grep(names(run), pattern = "V.")], 1, sum)
   run$Re <- apply(run, 1, function(x) calculate_Re(x, params))
   run$prob_outbreak_10 <- prob_outbreak_fcn(R = run$Re, outbreak_size = 10)
   
