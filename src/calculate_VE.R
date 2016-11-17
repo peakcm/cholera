39d7535e7c5825478ad13e85d97b4868c0ea2121
peakcm <coreypeak@gmail.com>
2016-11-17 15:39:43 -0500

    Add max_V_months to practice calls.

--- src/calculate_VE.R
+++ src/calculate_VE.R
@@ -42,5 +42,5 @@ Create_VE <- function(timesteps_per_month,VE_shape = "Shanchol",bound=TRUE, max_
 # Create Shancol and Dukoral Estimates
 # Shanchol <- matrix(c(0,6,12,18,24,30,36,42,48,.430,.525,.579,.583,.538,.438,.245,-.073,-.590),nrow=9)
 # Dukoral  <- matrix(c(0,6,12,18,24,30,36,42,48,.713,.650,.572,.476,.374,.280,.202,.141,.092),nrow=9)
-# VE_Shanchol <- Create_VE(timesteps_per_month = 1,VE_shape = "Shanchol",bound = TRUE)
-# VE_Dukoral  <- Create_VE(timesteps_per_month = 1,VE_shape = "Dukoral",bound = TRUE)
+# VE_Shanchol <- Create_VE(timesteps_per_month = 1,VE_shape = "Shanchol",bound = TRUE, max_V_months = 60)
+# VE_Dukoral  <- Create_VE(timesteps_per_month = 1,VE_shape = "Dukoral",bound = TRUE, max_V_months = 60)
