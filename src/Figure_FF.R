04109ab74c843579be4e91aeb847506299484140
peakcm <coreypeak@gmail.com>
2016-11-18 17:30:03 -0500

    Working version of figure that loops through conditions of interest and plots.

--- src/Figure_FF.R
+++ src/Figure_FF.R
@@ -29,86 +29,80 @@ years = 10
 times <- seq(0,356*years)
 
 N_pop <- 10000
-N_vax <- N_pop*10
+N_vax <- N_pop*3
 
-max_V_months = 12*(4)  # Go beyond "years" so the "perfect" vaccine remains perfect 
+max_V_months = 12*(4) 
 V_comps_per_month = 1
 n.comps.V = max_V_months*V_comps_per_month
 
+VE <- Create_VE(timesteps_per_month = V_comps_per_month, VE_shape = "Shanchol",bound = TRUE,max_V_months = max_V_months)
+
 #### Changing Conditions ####
-VE_conditions <- list(
-  "Shanchol" = Create_VE(timesteps_per_month = V_comps_per_month, VE_shape = "Shanchol", bound = TRUE, max_V_months = max_V_months)
-)
+vac_mass_frac_conditions <- c(1, 0.8, 0.7)     # This also applies to the initial mass coverage for "Mass" and "Mass_Maintain"
+vac_mass_freq_conditions <- c(365) # Applies only to "Mass"
+vac_routine_count_conditions <- c(round(N_vax/(365*years)), 1.5*round(N_vax/(365*years)), 2*round(N_vax/(365*years))) # Applies to "Routine" and "Mass_Maintain"
 
-R0_conditions <- c("Moderate" = 1.5, "High" = 2)
-mig_conditions <- c("low" = 1/(365*20), "high" = 1/(365*2))
+# mig_conditions <- c("low" = 1/(365*20), "moderate" = 1/(365*4.3), "high" = 1/(365*2)) # Applies to all
+mig_conditions <- c("high" = 1/(365*2))
 
-strategies <- c("Routine_1%", "Mass_100%_2yr","Mass_100%_routine_1%")
+strategies <- c("Routine", "Mass","Mass_Maintain")
 
-sims <- length(VE_conditions) * length(R0_conditions) * length(mig_conditions) * length(strategies)
+routine_sims <- length(vac_routine_count_conditions)*length(mig_conditions)
+mass_sims <- length(vac_mass_frac_conditions) * length(vac_mass_freq_conditions) * length(mig_conditions)
+mass_maintain_sims <- length(vac_mass_frac_conditions) * length(vac_routine_count_conditions) * length(mig_conditions)
+
+sims <- sum(routine_sims, mass_sims, mass_maintain_sims)
 
 #### Initialize data frame ####
-fig_FF_df <- data.table(VE_condition = rep(NA, sims), R0_condition = rep(NA, sims), mig_condition = rep(NA, sims), strategy = rep(NA, sims), Re = rep(NA, sims), prob_outbreak_10 = rep(NA, sims))
+fig_FF_df <- data.table(strategy = rep(NA, sims), vac_mass_frac_conditions = NA, vac_mass_freq_conditions = NA, vac_routine_count_conditions = NA, mig_conditions = NA, Re = NA, prob_outbreak_10 = NA, DHI = NA, Vax = NA)
 
-fig_FF_df$VE_condition_name <- rep(names(VE_conditions), each = sims/length(VE_conditions))
-fig_FF_df$R0_condition_name <- rep(names(R0_conditions), times = sims/length(R0_conditions))
-fig_FF_df$mig_condition_name <- rep(rep(names(mig_conditions), each = length(R0_conditions)), times = sims / (length(R0_conditions)*length(mig_conditions))) 
-fig_FF_df$strategy_name <- rep(rep(strategies, each = length(R0_conditions)*length(VE_conditions)), times = sims / (length(VE_conditions)*length(R0_conditions)*length(strategies))) 
+fig_FF_df$strategy <- c(rep(strategies[1], routine_sims), rep(strategies[2], mass_sims), rep(strategies[3], mass_maintain_sims))
 
-fig_FF_df$VE_condition <- rep(VE_conditions, each = sims/length(VE_conditions))
-fig_FF_df$R0_condition <- rep(R0_conditions, times = sims/length(R0_conditions)) 
-fig_FF_df$mig_condition <- rep(rep(mig_conditions, each = length(R0_conditions)), times = sims / (length(R0_conditions)*length(mig_conditions))) 
+fig_FF_df$vac_mass_frac_conditions <- c(rep(0, routine_sims), rep(vac_mass_frac_conditions, each = mass_sims/length(vac_mass_frac_conditions)), rep(vac_mass_frac_conditions, each = mass_maintain_sims/length(vac_mass_frac_conditions)))
+fig_FF_df$vac_mass_freq_conditions <- c(rep(0, routine_sims), rep(vac_mass_freq_conditions, times = mass_sims/length(vac_mass_freq_conditions)), rep(0, mass_maintain_sims))
+fig_FF_df$vac_routine_count_conditions <- c(rep(vac_routine_count_conditions, routine_sims/length(vac_routine_count_conditions)), rep(0, mass_sims), rep(vac_routine_count_conditions, times = mass_maintain_sims/length(vac_routine_count_conditions)))
+  
+fig_FF_df$mig_conditions <- rep(mig_conditions, sims)
 
 
 #### Loop through conditions ####
 for (row in seq_len(sims)){
   
-  if (fig_FF_df[row]$strategy_name == "Routine_1%"){
-    vac_routine_frac = 0.01
-    vac_mass_frac = 0
-    vac_mass_freq = 0
-    vac_birth_frac = 0
-    vac_mig_frac = 0
+    vac_routine_count = fig_FF_df$vac_routine_count_conditions[row]
+    vac_mass_frac = fig_FF_df$vac_mass_frac_conditions[row]
+    vac_mass_freq = fig_FF_df$vac_mass_freq_conditions[row]
+    init_frac = vac_mass_frac
+    mig_in = fig_FF_df$mig_condition[row]
+    mig_out = fig_FF_df$mig_condition[row]
+    
+  if (fig_FF_df[row]$strategy == "Routine"){
     vac_recip = c("routine_S")
-    init_frac = 0
-  } else if (fig_FF_df[row]$strategy_name == "Mass_100%_2yr"){
-    vac_routine_frac = 0
-    vac_mass_frac = 1
-    vac_mass_freq = 365*2
-    vac_birth_frac = 0
-    vac_mig_frac = 0
+  } else if (fig_FF_df[row]$strategy == "Mass"){
     vac_recip = c("mass_S")
-    init_frac = 1
-  } else if (fig_FF_df[row]$strategy_name == "Mass_100%_routine_1%"){
-    vac_routine_frac = 0.01
-    vac_mass_frac = 0
-    vac_mass_freq = 0
-    vac_birth_frac = 0
-    vac_mig_frac = 0
+  } else if (fig_FF_df[row]$strategy == "Mass_Maintain"){
     vac_recip = c("routine_S")
-    init_frac = 1
   }
   
   # Create params
-  params <- list(beta=fig_FF_df$R0_condition[row]/2,                # Daily transmission parameter. From Guinea, beta=0.6538415
-                 beta_shape = "sinusoidal",       # Shape of the seasonal forcing function. "constant" or "sinusoidal"
+  params <- list(beta=1.5/2,                # Daily transmission parameter. From Guinea, beta=0.6538415
+                 beta_shape = "constant",       # Shape of the seasonal forcing function. "constant" or "sinusoidal"
                  beta_amp = 0.00,               # Amplitude of sinusoidal seasonal forcing function (0 if no change, 1 if doubles)
                  beta_phase_shift = 0,          # Phase shift in a sinusoidal seasonal forcing function
                  gamma=1/2,                     # Duration of disease
                  sigma=1/1.4,                   # Incubation period
                  birth_death_rate=1/(365*40), # Average birth and death rate
                  nat_wane=0*1/(365*10),         # Rate of natural immunity waning
-                 mig_in= fig_FF_df$mig_condition[row],             # Rate of immigration
-                 mig_out=fig_FF_df$mig_condition[row],             # Rate of emigration
+                 mig_in= mig_in,             # Rate of immigration
+                 mig_out= mig_out,             # Rate of emigration
                  foreign_infection=0.00,        # Proportion of immigrants who are infected
                  n.comps.V=n.comps.V,           # Number of V compartments
-                 VE=fig_FF_df$VE_condition[row][[1]],                         # Vaccine efficacy over time
+                 VE= VE,                         # Vaccine efficacy over time
                  V_step=V_comps_per_month/30.5, # Average time in each vaccine compartment is one month
-                 vac_routine_frac = vac_routine_frac,          # Fraction of the current S pop that is vaccinated on each day
+                 vac_routine_count = vac_routine_count,          # Fraction of the current S pop that is vaccinated on each day
                  vac_mass_freq = vac_mass_freq,         # Days between mass re-vaccination campaigns
                  vac_mass_frac = vac_mass_frac,           # Fraction of the population revaccinated during mass revaccination campaigns
-                 vac_birth_frac = vac_birth_frac,            # Fraction of babies vaccinated
-                 vac_mig_frac = vac_mig_frac,            # Fraction of immigrants vaccinated upon arrival
+                 vac_birth_frac = 0,            # Fraction of babies vaccinated
+                 vac_mig_frac = 0,            # Fraction of immigrants vaccinated upon arrival
                  vac_max = N_vax,                 # Maximum number of vaccines to be given
                  vac_recip = vac_recip  # Recipients of vaccination ("all", "S", "migrant", "birth")
   )
@@ -123,6 +117,9 @@ for (row in seq_len(sims)){
   
   fig_FF_df$Re[row] <- list(output$Re)
   fig_FF_df$prob_outbreak_10[row] <- list(output$prob_outbreak_10)
+  fig_FF_df$Vax[row] <- list(output$Vax)
+  
+  fig_FF_df$DHI[row] <- (sum(fig_FF_df$Re[row][[1]] <= 1) - 1)/365
   
   cat(".")
 }
@@ -130,31 +127,31 @@ for (row in seq_len(sims)){
 plot(fig_FF_df$Re[[1]], type = "l")
 plot(fig_FF_df$prob_outbreak_10[[1]], type = "l")
 
-#### Calculate DHI ####
-fig_FF_df$DHI <- NA
-
-for (row in 1:nrow(fig_FF_df)){
-  fig_FF_df$DHI[row] <- sum(fig_FF_df$Re[row][[1]] <= 1) - 1
-}
-
-#### Manually melt data frame ####
-fig_FF_df_melt <- data.frame(times = rep(times, sims), Re = NA, prob_outbreak_10 = NA,  VE_condition_name = NA, R0_condition_name = NA, mig_condition_name = NA, DHI = FALSE)
+#### Manually melt to a data frame ####
+fig_FF_df_melt <- data.frame(times = rep(times, sims), Re = NA, prob_outbreak_10 = NA,  strategy = NA, vac_mass_frac_condition = NA, vac_mass_freq_condition = NA, vac_routine_count_condition = NA, mig_condition = NA, DHI = FALSE)
 
 for (row in 1:sims){
-  VE_condition_name = fig_FF_df$VE_condition_name[[row]]
-  R0_condition_name = fig_FF_df$R0_condition_name[[row]]
-  mig_condition_name = fig_FF_df$mig_condition_name[[row]]
+  strategy = fig_FF_df$strategy[[row]]
+  vac_mass_frac_condition = fig_FF_df$vac_mass_frac_conditions[[row]]
+  vac_mass_freq_condition = fig_FF_df$vac_mass_freq_conditions[[row]]
+  vac_routine_count_condition = fig_FF_df$vac_routine_count_conditions[[row]]
+  mig_condition = fig_FF_df$mig_conditions[[row]]
   
-  fig_FF_df_melt[(length(times)*(row-1)+1):(length(times)*row), c("VE_condition_name")] <- VE_condition_name
-  fig_FF_df_melt[(length(times)*(row-1)+1):(length(times)*row), c("R0_condition_name")] <- R0_condition_name
-  fig_FF_df_melt[(length(times)*(row-1)+1):(length(times)*row), c("mig_condition_name")] <- mig_condition_name
+  
+  fig_FF_df_melt[(length(times)*(row-1)+1):(length(times)*row), c("strategy")] <- strategy
+  fig_FF_df_melt[(length(times)*(row-1)+1):(length(times)*row), c("vac_mass_frac_condition")] <- vac_mass_frac_condition
+  fig_FF_df_melt[(length(times)*(row-1)+1):(length(times)*row), c("vac_mass_freq_condition")] <- vac_mass_freq_condition
+  fig_FF_df_melt[(length(times)*(row-1)+1):(length(times)*row), c("vac_routine_count_condition")] <- vac_routine_count_condition
+  fig_FF_df_melt[(length(times)*(row-1)+1):(length(times)*row), c("mig_condition")] <- mig_condition
   
   fig_FF_df_melt[(length(times)*(row-1)+1):(length(times)*row), c("Re")] <- fig_FF_df$Re[[row]]
   fig_FF_df_melt[(length(times)*(row-1)+1):(length(times)*row), c("prob_outbreak_10")] <- fig_FF_df$prob_outbreak_10[[row]]
   
-  fig_FF_df_melt[fig_FF_df_melt$VE_condition_name %in% fig_FF_df$VE_condition_name[row] &
-                   fig_FF_df_melt$R0_condition_name %in% fig_FF_df$R0_condition_name[row] &
-                   fig_FF_df_melt$mig_condition_name %in% fig_FF_df$mig_condition_name[row] &
+  fig_FF_df_melt[fig_FF_df_melt$strategy %in% fig_FF_df$strategy[row] &
+                   fig_FF_df_melt$vac_mass_frac_condition %in% fig_FF_df$vac_mass_frac_conditions[row] &
+                   fig_FF_df_melt$vac_mass_freq_condition %in% fig_FF_df$vac_mass_freq_conditions[row] &
+                   fig_FF_df_melt$vac_routine_count_condition %in% fig_FF_df$vac_routine_count_conditions[row] &
+                   fig_FF_df_melt$mig_condition %in% fig_FF_df$mig_conditions[row] &
                    fig_FF_df_melt$times == fig_FF_df$DHI[row], "DHI"] <- TRUE
   #     fig_FF_df_melt[fig_FF_df_melt$VE_condition_name %in% fig_FF_df$VE_condition_name[row] &
   #                     fig_FF_df_melt$R0_condition_name %in% fig_FF_df$R0_condition_name[row] &
@@ -163,11 +160,14 @@ for (row in 1:sims){
   cat(".")
 }
 
-# Rename Factors
-fig_FF_df_melt$VE_condition_name <- factor(fig_FF_df_melt$VE_condition_name, levels = c("Shanchol", "Dukoral", "Perfect"), labels = c("Whole Cell\n(eg Shanchol)", "BS-Whole Cell\n(eg Dukoral)", "Perfect Vaccine"), ordered = TRUE)
-fig_FF_df_melt$mig_condition_name <- factor(fig_FF_df_melt$mig_condition_name, levels = c("high", "low", "none"), labels = c("High", "Low", "None"), ordered = TRUE)
-fig_FF_df_melt$R0_condition_name <- factor(fig_FF_df_melt$R0_condition_name, levels = c("High", "Moderate", "Low"), labels = c("High (2)", "Moderate (1.5)", "Low (1)"), ordered = TRUE)
+# # Rename Factors
+# fig_FF_df_melt$VE_condition_name <- factor(fig_FF_df_melt$VE_condition_name, levels = c("Shanchol", "Dukoral", "Perfect"), labels = c("Whole Cell\n(eg Shanchol)", "BS-Whole Cell\n(eg Dukoral)", "Perfect Vaccine"), ordered = TRUE)
+# fig_FF_df_melt$mig_condition_name <- factor(fig_FF_df_melt$mig_condition_name, levels = c("high", "low", "none"), labels = c("High", "Low", "None"), ordered = TRUE)
+# fig_FF_df_melt$R0_condition_name <- factor(fig_FF_df_melt$R0_condition_name, levels = c("High", "Moderate", "Low"), labels = c("High (2)", "Moderate (1.5)", "Low (1)"), ordered = TRUE)
 
 #### Plot Re over time ####
 # The blue dotted line essentially shows the assume vaccine waning profile.
-ggplot(fig_FF_df_melt, aes(x = times/365, y = Re, linetype = mig_condition_name, color = R0_condition_name)) + geom_hline(yintercept =1, col = "darkgrey") + geom_line() + facet_grid(VE_condition_name ~.) + xlab("Years since Vaccination") + ylab("Effective Reproductive Number") + theme_bw() + scale_color_discrete(name = "Basic Reproductive\nNumber") + scale_linetype_discrete(name = "In/Out Migration Rate") + ylim(0,2) + theme(text = element_text(size=6), legend.text=element_text(size=6), legend.title=element_text(size=6))
+ggplot(fig_FF_df_melt[fig_FF_df_melt$vac_mass_freq_condition != 730 & fig_FF_df_melt$vac_mass_frac_condition %in% c(0, 0.8, 1),], aes(x = times/365, y = Re, color = factor(vac_routine_count_condition), linetype = factor(vac_mass_frac_condition ))) + geom_hline(yintercept = 1, col = "darkgrey") + geom_line() + facet_grid(strategy ~.) + xlab("Years since Vaccination") + ylab("Effective Reproductive Number") + theme_bw() + ylim(0,2) + theme(text = element_text(size=6), legend.text=element_text(size=6), legend.title=element_text(size=6)) + scale_linetype_manual(name = "Mass Vaccination Coverage", values = c("dotted", "longdash", "solid")) + scale_color_discrete(name = "Routine Doses per Day") 
+
+ggsave(file = "figures/Figure_FF.pdf", width = 5, height = 3, units = "in")
+ 
