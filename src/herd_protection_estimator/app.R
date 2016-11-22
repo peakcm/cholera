fec743d98989ef20a71585947ebd4ad8bb1cb497
peakcm <coreypeak@gmail.com>
2016-11-22 08:41:09 -0500

    - Update app to match variable names. - Simplify app so that vaccine repicient groups are inferred from the parameters instead of explicitly defined

--- src/herd_protection_estimator/app.R
+++ src/herd_protection_estimator/app.R
@@ -51,12 +51,6 @@ ui <- fluidPage(
                    max = 3,
                    value = 1.25,
                    step = 0.05),
-       sliderInput("VaxCov",
-                   "Initial Vaccine Coverage:",
-                   min = 0,
-                   max = 1,
-                   value = 1,
-                   step = 0.01),
        radioButtons("VE_shape", 
                     "Vaccine:",
                     choices = c("Dukoral", "Shanchol"),
@@ -90,34 +84,42 @@ ui <- fluidPage(
                    value = 0.2,
                    step = 0.1)),
     column(3, offset = 1,
-         checkboxGroupInput("vac_recip",
-                            "Ongoing Vaccine Recipients:", 
-                            choices = c("Births" = "birth", "Migrants" = "migrant", "Mass for Susceptibles" = "mass_S", "Mass for All" = "mass_all"),
-                            selected = c()),
+           sliderInput("VaxCov",
+                       "Initial Vaccine Coverage:",
+                       min = 0,
+                       max = 1,
+                       value = 1,
+                       step = 0.01),
+         sliderInput("vac_routine_count",
+                     "Number of Routine Vaccine Courses per Day:",
+                     min = 0,
+                     max = 200,
+                     value = 0,
+                     step = 10),
          sliderInput("vac_mass_freq",
                  "Frequency of Mass Vaccination (years):",
                  min = 0,
                  max = 10,
-                 value = 2,
+                 value = 0,
                  step = 1),
-       sliderInput("vac_mass_frac",
+        sliderInput("vac_mass_frac",
                    "Fraction Vaccinated during Mass Campaigns:",
                    min = 0,
                    max = 1,
                    value = 1,
                    step = 0.01),
-       sliderInput("vac_birth_frac",
-                   "Fraction Vaccinated (Birth):",
-                   min = 0,
-                   max = 1,
-                   value = 1,
-                   step = 0.01),
-       sliderInput("vac_mig_frac",
-                   "Fraction Vaccinated (Immigrants):",
-                   min = 0,
-                   max = 1,
-                   value = 1,
-                   step = 0.01),
+       # sliderInput("vac_birth_frac",
+       #             "Fraction Vaccinated (Birth):",
+       #             min = 0,
+       #             max = 1,
+       #             value = 1,
+       #             step = 0.01),
+       # sliderInput("vac_mig_frac",
+       #             "Fraction Vaccinated (Immigrants):",
+       #             min = 0,
+       #             max = 1,
+       #             value = 1,
+       #             step = 0.01),
        sliderInput("vac_max",
                    "Maximum Vaccine Courses Allocated:",
                    min = 0,
@@ -156,14 +158,14 @@ server <- function(input, output) {
                      n.comps.V=n.comps.V,           # Number of V compartments
                      VE=VE,                         # Vaccine efficacy over time
                      V_step=V_comps_per_month/30.5, # Average time in each vaccine compartment is one month
-                     vac_routine_frac = 0,          # Fraction of the current S pop that is vaccinated on each day
+                     vac_routine_count = input$vac_routine_count,         # Number of courses given each day
                      
                      vac_mass_freq = round(input$vac_mass_freq*365),                  # Days between re-vaccination campaigns
                      vac_mass_frac = input$vac_mass_frac,
-                     vac_birth_frac = input$vac_birth_frac,            # Fraction of babies vaccinated
-                     vac_mig_frac = input$vac_mig_frac,            # Fraction of immigrants vaccinated upon arrival
+                     vac_birth_frac = 0,            # Fraction of babies vaccinated
+                     vac_mig_frac = 0,            # Fraction of immigrants vaccinated upon arrival
                      vac_max = input$vac_max,                 # Maximum number of vaccines to be given
-                     vac_recip = input$vac_recip  # Recipients of vaccination ("all", "S", "migrant", "birth")
+                     vac_recip = c("routine_S", "mass_all")  # Recipients of vaccination ("all", "S", "migrant", "birth")
       )
       inits = rep(0, 7+params$n.comps.V)
       inits[1] = N*(1-input$VaxCov) # initially susceptible
@@ -194,14 +196,14 @@ server <- function(input, output) {
                      VE=VE,                         # Vaccine efficacy over time
                      V_step=V_comps_per_month/30.5, # Average time in each vaccine compartment is one month
                      
-                     vac_routine_frac = 0,          # Fraction of the current S pop that is vaccinated on each day
+                     vac_routine_count = 0,          # Fraction of the current S pop that is vaccinated on each day
                      
                      vac_mass_freq = 0,                  # Days between re-vaccination campaigns
                      vac_mass_frac = 0,
                      vac_birth_frac = 0,            # Fraction of babies vaccinated
                      vac_mig_frac = 0,            # Fraction of immigrants vaccinated upon arrival
                      vac_max = 0,                 # Maximum number of vaccines to be given
-                     vac_recip = input$vac_recip  # Recipients of vaccination ("all", "S", "migrant", "birth")
+                     vac_recip = c("routine_S", "mass_S")  # Recipients of vaccination ("all", "S", "migrant", "birth")
       )
       inits = rep(0, 7+params$n.comps.V)
       inits[1] = N # initially susceptible
