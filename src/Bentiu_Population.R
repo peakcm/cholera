20bec96ea5716ef5fc1b2a996666ac403e174435
peakcm <coreypeak@gmail.com>
2016-12-10 12:09:38 -0500

    Update Bentiu figure

--- src/Bentiu_Population.R
+++ src/Bentiu_Population.R
@@ -36,15 +36,17 @@ ggplot(df) +
   # geom_rect(data = df_rect, aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax), fill = "white", color = "black") +
   # geom_bar(data = df_cases, aes(x = Date, y = Cases_weekly*500), stat = "identity", width = 2) + 
   geom_bar(aes(x = Date, y = Cholera.cases*1000), stat = "identity", fill = "lightgrey") + 
-  geom_text(aes(x = Date, y = (Cholera.cases*1000 + 3000), label = Cholera.cases), size = 2) + 
+  geom_text(aes(x = Date, y = (Cholera.cases*1000), label = paste(Cholera.cases, "\nCases")), size = 2) + 
   geom_ribbon(data = df[is.na(df$influx)==0,], aes(x = Date, ymin = pop, ymax = influx), fill = "lightgreen") +
   geom_ribbon(data = df[is.na(df$outflux)==0,], aes(x = Date, ymin = outflux, ymax = pop), fill = "pink") +
   geom_line(aes(x = Date, y = pop), color = "darkblue") +
-  geom_bar(data = df[is.na(df$Vaccine.Doses)==0,], aes(x = Date, y = Vaccine.Doses/2), stat = "identity", width = 6, color = "forestgreen") +
+  geom_bar(data = df[is.na(df$Vaccine.Doses)==0,], aes(x = Date, y = Vaccine.Doses/2), stat = "identity", width = 6, color = "forestgreen", alpha = .6) +
+  geom_text(data = df[is.na(df$Vaccine.Doses)==0,], aes(x = (Date + 90), y = (Vaccine.Doses/2 - 2000), label = paste(Vaccine.Doses, "\nDoses")), color = "forestgreen", size = 2) +
   scale_y_continuous(breaks = c(0, 5e4, 1e5, 1.5e5), labels = c("0", "50", "100", "150"), name = "Population (thousands)") +
-  scale_x_date()
   theme_bw() +
   theme(text = element_text(size = 6), legend.text=element_text(size=4), axis.text.x = element_text(size = 6), axis.text.y = element_text(size = 6))  +
+  scale_x_date(date_labels = "%b '%y") + 
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
   ggtitle("Bentiu PoC Camp")
 
 ggsave(file = "figures/Figure_GG.pdf", width = 3, height = 3, units = "in")
