0e7cd364431bbc7a741ac0d62fedddb6798acbd5
peakcm <coreypeak@gmail.com>
2016-12-23 21:13:01 -0500

    Fixed a bug

--- src/migration_rate_calculator.R
+++ src/migration_rate_calculator.R
@@ -1,5 +1,6 @@
 #### Function to update migration rates ####
 migration_rate_calculator <- function(t, rate_vector){
-  t_floor <- floor(t)
+  t_floor <- max(1,floor(t))
   rate_vector[t_floor]
 }
+
