d83b3f6438f21d02d3a801ca9eacacc08973a4d8
peakcm <coreypeak@gmail.com>
2017-01-30 09:14:25 -0500

    Revise outbreak size calculation to include at least Y cases

--- src/prob_outbreak_fcn.R
+++ src/prob_outbreak_fcn.R
@@ -6,7 +6,7 @@
 prob_outbreak_fcn <- function(R, outbreak_size=10, distribution = "Poisson"){
   if (distribution == "Poisson"){
     out = 0
-    for (y in seq_len(outbreak_size)){
+    for (y in seq_len(outbreak_size-1)){
       out <- out + 1/factorial(y-1) * y^(y-2) * R^(y-1) * exp(-y*R)
 
     }
@@ -15,7 +15,7 @@ prob_outbreak_fcn <- function(R, outbreak_size=10, distribution = "Poisson"){
 }
 
 # par(mar = c(6,6,6,6))
-# plot(seq(0, 3, by=0.01), prob_outbreak_fcn(seq(0, 3, by=0.01), outbreak_size = 5), type="l", ylim = c(0,1), col = "darkgreen", xlab = "R", ylab = "Probability of an Outbreak Greater Than...\n5, 10, or 50 cases")
+# plot(seq(0, 3, by=0.01), prob_outbreak_fcn(seq(0, 3, by=0.01), outbreak_size = 5), type="l", ylim = c(0,1), col = "darkgreen", xlab = "R", ylab = "Probability of an Outbreak of at least...\n5, 10, or 50 cases")
 # lines(seq(0, 3, by=0.01), prob_outbreak_fcn(seq(0, 3, by=0.01), outbreak_size = 10), col = "orange")
 # lines(seq(0, 3, by=0.01), prob_outbreak_fcn(seq(0, 3, by=0.01), outbreak_size = 50), col = "darkred")
 
