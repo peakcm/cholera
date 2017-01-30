bc034c93567c560d46b41b0a737262f79418c57a
peakcm <coreypeak@gmail.com>
2017-01-30 10:11:23 -0500

    Test performance of cum_prob_outbreak function

--- src/vax_targeting.R
+++ src/vax_targeting.R
@@ -40,14 +40,23 @@ cum_prob_outbreak <- function(years, mig_rate, pop_size, time_step, avg_prob, se
     Xt <- c(1 - (VE_t*exp(-1*(mig_rate/365)*(1:(length(VE_t))))), rep(1, times = (365*years - length(VE_t))))
   }
   
-  df <- data.frame(days = 1:(365*years), seasonal_time = rep(seq_len(365), years), Xt = Xt, Nt = NA, Ct = NA, Rt = NA, prob_outbreak_t = NA)
+  df <- data.frame(days = 1:(365*years), seasonal_time = rep(seq_len(365), years), Xt = Xt, Nt = NA, Ct = NA, Rt = NA, prob_outbreak_t_per_inf = NA, prob_outbreak_t = NA)
 
   for (day in 1:nrow(df)){
     df[day, "Nt"] <- expected_migrants(mig_rate = mig_rate, pop_size = pop_size, time_step = time_step)
     df[day, "Ct"] <- df[day, "Nt"] * prob_migrant_infected(avg_prob = avg_prob, seasonal_amp = seasonal_amp, seasonal_time = df[day, "seasonal_time"])
     df[day, "Rt"] <- R * (1+seasonal_amp*cos(2*pi*df[day, "seasonal_time"]/365)) * df[day, "Xt"]
+    df[day, "prob_outbreak_t_per_inf"] <- prob_outbreak_fcn(R = df[day,"Rt"], outbreak_size = outbreak_size)
     df[day, "prob_outbreak_t"] <- 1-(1-prob_outbreak_fcn(R = df[day,"Rt"], outbreak_size = outbreak_size))^df[day, "Ct"]
   }
 
   return(1-prod((1-df[, "prob_outbreak_t"])))
 }
+
+#### Test to make sure this method works for fractional cases on each day ####
+df_test <- data.frame(day = 1:6, Rt =1.1, Ct = c(1,1,0.5, 0.5, 0.5, 0.5), prob_outbreak_t = NA)
+for (i in seq_len(nrow(df_test))){df_test[i, "prob_outbreak_t"] <- 1-(1-prob_outbreak_fcn(R = df_test[i,"Rt"], outbreak_size = outbreak_size))^df_test[i, "Ct"]}
+df_test
+1-prod(1-df_test[1:2, "prob_outbreak_t"])
+1-prod(1-df_test[3:6, "prob_outbreak_t"])
+0
