935b58843c25aee27f86eee2362d19bf273488cd
peakcm <coreypeak@gmail.com>
2017-01-31 10:41:54 -0500

    Add tests for final size

--- src/final_epidemic_size.R
+++ src/final_epidemic_size.R
@@ -1,4 +1,5 @@
-# Adapted from http://www.sciencedirect.com/science/article/pii/S0022519314006882
+# Adapted from [Black and Ross 2015 J Theoretical Biology]
+# http://www.sciencedirect.com/science/article/pii/S0022519314006882
 
 final_epidemic_size <- function(N, R0){
   
@@ -20,9 +21,19 @@ final_epidemic_size <- function(N, R0){
 }
 
 # Test function behavior
-# q <- final_epidemic_size(1000, 1.1)
+# N = 20000
+# R = 1.5
+# q <- final_epidemic_size(N, R)
 # cat("The probability of <10 cases is",sum(q[1:10]))
-# cat("The probability of 10 to 50 cases is", sum(q[11:51]))
-# cat("The probability of >50 cases is", sum(q[52:N]))
-# hist(q*N)
-
+# cat("The probability of 10 to 50 cases is", sum(q[11:(N/3)]))
+# cat("The probability of >50 cases is", sum(q[(N/3):N]))
+# plot(q*N, type = "h")
+# plot(q*N, type = "h", xlim = c(0,30))
+# 
+# # Probability of a large outbreak (>1% AR) given you make it past "cut_point" cases
+# cut_point <- 10
+# sum(q[(N/100):N]) / sum(q[(cut_point+1):N])
+# When N=20,000 and R=1.5, cut_point 10 is 89% predictive. cut_point 25 is 97% predictive
+# When N=10,000 and R=1.5, cut_point 10 is 88% predictive. cut_point 25 is 97% predictive
+# When N=10,000 and R=1.4, cut_point 10 is 82% predictive. cut_point 25 is 95% predictive
+# When N=10,000 and R=1.1, cut_point 10 is 47% predictive. cut_point 25 is 68% predictive. cut_point 80 is 95% predictive....but not very interesting 
